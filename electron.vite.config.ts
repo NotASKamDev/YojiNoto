5c11aee6c0f575a5cfb50ba6a2e245537b3eb819
NotASKamDev <106349029+NotASKamDev@users.noreply.github.com>
2024-09-21 18:46:22 -0400

    Hello, World

---
+++ electron.vite.config.ts
@@ -0,0 +1,27 @@
+import { resolve } from 'path'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+    build:{
+      rollupOptions:{
+        output:{
+          format:"es"
+        }
+      }
+    }
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src')
+      }
+    },
+    plugins: [vue()]
+  }
+})
---
+++ src/renderer/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// import Versions from './components/Versions.vue'
+
+// const ipcHandle = () => {
+//   console.log("ping")
+//   window.electron.ipcRenderer.send('ping')
+// }
+
+</script>
+
+<template>
+  hello  world
+</template>
